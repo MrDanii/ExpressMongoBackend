0d7edeb7a50dd862be37aabc7db029c9153cd7ad
MrDanii <dan333_d@hotmail.com>
2021-03-05 22:17:08 -0600

    SPA Control

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const cors = require('cors')
 const { dbConnection } = require('./database/config');
+const path = require('path')
 
 // Crear Servidor
 const app = express();
@@ -26,6 +27,11 @@ app.use('/api/medicos', require('./routes/medicos'))
 app.use('/api/login', require('./routes/auth'))
 app.use('/api/todo', require('./routes/busquedas'))
 app.use('/api/upload', require('./routes/uploads'))
+
+// Comodin page
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running in port " + process.env.PORT)
